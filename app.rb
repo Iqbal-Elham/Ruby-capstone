d2aec1355070e2f80d63fbdd21b88131a3cb321a
cabraham1 <christopherabraham8@gmail.com>
2023-05-30 20:23:55 +0100

    FIx linters error

--- app.rb
+++ app.rb
@@ -1,6 +1,6 @@
-require './classes/genre'
-require './classes/music'
-require json
+require 'json'
+require_relative './classes/genre'
+require_relative './classes/music'
 
 class Application
   attr_reader :genres, :music_albums
@@ -10,10 +10,10 @@ class Application
     @music_albums = []
   end
 
-  # genre part
+  # Genre part
 
   def add_genre(item)
-    print "Enter genre name: "
+    print 'Enter genre name: '
     name = gets.chomp
 
     genre = Genre.new(name)
@@ -24,37 +24,42 @@ class Application
   def store_genre(genre)
     hash = { id: genre.id, name: genre.name }
 
-    file = File.read('./data/genre_list.json').zero? ? [] : JSON.parse(file.read('./data/genre_list.json'))
+    file = File.empty?('./data/genre_list.json') ? [] : JSON.parse(File.read('./data/genre_list.json'))
     file.push(hash)
-    file.write('./data/genre_list.json', JSON.pretty_generate(file))
+    File.write('./data/genre_list.json', JSON.pretty_generate(file))
   end
 
   def list_all_genres
-    puts "List of all genres:"
-    file = File.read('./data/genre_list.json').zero? ? [] : JSON.parse(file.read('./data/genre_list.json'))
+    puts 'List of all genres:'
+    file = File.empty?('./data/genre_list.json') ? [] : JSON.parse(File.read('./data/genre_list.json'))
     file.each do |genre|
-      puts "Genre: #{genre.id} - #{genre['name']}"
+      puts "Genre: #{genre['id']} - #{genre['name']}"
     end
   end
 
   # Music part
 
   def add_music_album
-    print "Is the music on sportify? (Y/N): "
+    print 'Is the music on sportify? (Y/N): '
     sportify_value = gets.chomp.downcase == 'y'
-    print "Enter publish date (YYYY-MM-DD): "
+    print 'Enter publish date (YYYY-MM-DD): '
     publish_date = gets.chomp
     music_album = Music.new(publish_date, sportify_value)
-    add_genre(music)
-    puts "Music album added!"
-    store_music_album(music)
-    music
+    add_genre(music_album)
+    puts 'Music album added!'
+    store_music_album(music_album)
+    music_album
   end
 
-  def store_music(music)
-    new_music = { id: music.id, publish_date: music.publish_date, sportify: music.on_sportify, genre_id: music.genre.name }
+  def store_music_album(music)
+    new_music = {
+      id: music.id,
+      publish_date: music.publish_date,
+      sportify: music.on_sportify,
+      genre_id: music.genre.name
+    }
     if File.exist?('./data/music_list.json')
-      file = File.size('./data/music_list.json').zero? ? [] : JSON.parse(File.read('./data/music_list.json'))
+      file = File.empty?('./data/music_list.json') ? [] : JSON.parse(File.read('./data/music_list.json'))
       file.push(new_music)
       File.write('./data/music_list.json', JSON.pretty_generate(file))
     else
@@ -62,9 +67,9 @@ class Application
     end
   end
 
-    def list_all_music_albums
+  def list_all_music_albums
     puts "Music album's list"
-    musics = File.size('./data/music_list.json').zero? ? [] : JSON.parse(File.read('./data/music_list.json'))
+    musics = File.empty?('./data/music_list.json') ? [] : JSON.parse(File.read('./data/music_list.json'))
     musics.each do |music|
       puts "Published date: #{music['publish_date']}, On sportify: #{music['sportify']}, Genre: #{music['genre_id']}"
     end
