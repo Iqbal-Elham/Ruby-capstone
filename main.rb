6de8397591116c4fd9d21369b0579db261bde60a
cabraham1 <christopherabraham8@gmail.com>
2023-05-30 21:29:52 +0100

    Add list of main menu

--- main.rb
+++ main.rb
@@ -4,3 +4,20 @@ class Main
   def initialize
     @app = Application.new
   end
+
+  def manu
+    $stdout.sync = true
+    puts 'Catalogue of my many things console Application'
+    puts "
+    Please choose an option from the list by entering a number:\n
+    1 - List all books
+    2 - Add a book
+    3 - List all games
+    4 - Add a game
+    5 - Add a music album
+    6 - List all music albums
+    7 - List all genres
+    8 - List all labels
+    9 - List all authors
+    10 - Exit "
+end
