c7b3829ef2fda536b3ef5882c57900c8b03ad731
cabraham1 <christopherabraham8@gmail.com>
2023-05-30 10:22:53 +0100

    Add initialization to item class

--- classes/item.rb
+++ classes/item.rb
@@ -3,3 +3,10 @@ require 'securerandom'
 class Item
   attr_accessor :genre, :author, :label, :publish_date
   attr_reader :id, :archived
+
+  def initialize(publish_date)
+    @id = id || SecureRandom.uuid
+    @publish_date = publish_date
+    @archived = false
+  end
+end
