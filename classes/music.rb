e6a852b3898ed47b863e7bd9bb1af330dda6aec6
cabraham1 <christopherabraham8@gmail.com>
2023-05-30 10:01:17 +0100

    initialize music class

--- classes/music.rb
+++ classes/music.rb
@@ -3,4 +3,9 @@ require 'securerandom'
 class Music
   attr_accessor :name, :items
   attr_reader :id
+
+  def initialize(publish_date, on_sportify)
+    super(publish_date)
+    @on_sportify = on_sportify
+  end
 end
