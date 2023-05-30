45923c38fd427cb714e97b0e38d52ef2f4da02b2
cabraham1 <christopherabraham8@gmail.com>
2023-05-30 09:58:48 +0100

    initialize genre class

--- classes/genre.rb
+++ classes/genre.rb
@@ -3,4 +3,10 @@ require 'securerandom'
 class Genre
   attr_accessor :name, :items
   att_reader :id
+
+  def initialize(name)
+    @name = name
+    @id = SecureRandom.uuid
+    @items = []
+  end
 end
