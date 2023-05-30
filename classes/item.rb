b847a649134117b9178943a009efa275fa6f9d7d
cabraham1 <christopherabraham8@gmail.com>
2023-05-30 11:44:47 +0100

    Add all requirement for the item classs

--- classes/item.rb
+++ classes/item.rb
@@ -1,5 +1,4 @@
 require 'securerandom'
-
 class Item
   attr_accessor :genre, :author, :label, :publish_date
   attr_reader :id, :archived
@@ -8,5 +7,21 @@ class Item
     @id = id || SecureRandom.uuid
     @publish_date = publish_date
     @archived = false
+  end
+
+  def move_to_archive
+    @archived = true if can_be_archived?
+  end
+
+  def add_genre(genre)
+    genre.items << self unless genre.items.include?(self)
+    @genre = genre
+  end
+
+  private
+
+  def can_be_archived?
+    # return true if publish_date is older than 10 years
+    return true if @publish_date < Date.today - 3650
   end
 end
