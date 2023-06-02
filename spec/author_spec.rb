18785e8cbacdab6a5945bd4da8266dbfd0cb5622
Timothy Njoroge <timothynjoroge56@gmail.com>
2023-06-02 10:25:14 +0300

    Add unit tests for public methods

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -3,6 +3,7 @@ require_relative 'spec_helper'
 describe Author do
   before(:each) do
     @author = Author.new('John', 'Doe')
+    @game = Game.new(false, '2022-05-28')
   end
 
   it 'is an instance of Author' do
@@ -15,5 +16,10 @@ describe Author do
 
   it 'author has a lastname' do
     expect(@author.last_name).to eq 'Doe'
+  end
+
+  it 'add item method adds an item to the items array' do
+    @author.add_item(@game)
+    expect(@author.items.length).to be > 0
   end
 end
